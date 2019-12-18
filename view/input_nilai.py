6ce03ac2acd323d63ccd102091b9d2b184db45d2
awalhadi006 <awalhadi006@gmail.com>
2019-12-18 21:37:55 +0700

    Tugas Praktikum 7

---
+++ view/input_nilai.py
@@ -0,0 +1,21 @@
+nim = []
+nama = []
+tugas = []
+uts = []
+nakhir = []
+uas = []
+
+
+def input_data():
+    masnim = input("NIM\t\t\t : ")
+    nim.append({'nim': masnim})
+    masnama = input("NAMA\t\t : ")
+    nama.append({'nama': masnama})
+    masuts = int(input("Nilai UTS\t : "))
+    uts.append({'uts': masuts})
+    masuas = int(input("Nilai UAS\t : "))
+    uas.append({'uas': masuas})
+    mastugas = int(input("Nilai Tugas\t : "))
+    tugas.append({'tugas': mastugas})
+    akhir = (30 * mastugas / 100) + (35 * masuts / 100) + (35 * masuas / 100)
+    nakhir.append({'nakhir': akhir})
